514d36289ed982d852f7b491449763fb89309331
hubery <hubery.kung@gmail.com>
2025-04-24 15:27:52 +0800

    ci: add revia cicd
    fix: update chart demo

---
+++ scripts/build-revia-docker.sh
@@ -0,0 +1,133 @@
+#!/usr/bin/env bash
+
+if [[ -z "${SERVER_DOCKERHUB_REPOSITORY}" ]]; then
+  echo "SERVER_DOCKERHUB_REPOSITORY is missing"
+  exit 1
+fi
+
+if [[ -z "${GITHUB_SHA}" ]]; then
+  echo "GITHUB_SHA is missing"
+  exit 1
+fi
+
+# Fail on error
+set -e
+
+# Echo commands
+set -x
+
+# Build server tarball
+tar \
+  --no-xattrs \
+  --exclude='*.ts' \
+  --exclude='*.tsbuildinfo' \
+  -czf medplum-server.tar.gz \
+  package.json \
+  package-lock.json \
+  packages/ccda/package.json \
+  packages/ccda/dist \
+  packages/core/package.json \
+  packages/core/dist \
+  packages/definitions/package.json \
+  packages/definitions/dist \
+  packages/fhir-router/package.json \
+  packages/fhir-router/dist \
+  packages/server/package.json \
+  packages/server/dist \
+  packages/server/static
+
+# Build app tarball
+# The -C flag rewrites the base path from packages/app/dist/ to ./
+tar \
+  --no-xattrs \
+  -czf ./packages/app/medplum-app.tar.gz \
+  -C packages/app/dist .
+
+# Build foomedical tarball
+# The -C flag rewrites the base path from packages/app/dist/ to ./
+tar \
+  --no-xattrs \
+  -czf ./examples/foomedical/medplum-foomedical.tar.gz \
+  -C examples/foomedical/dist .
+
+# Build provider tarball
+# The -C flag rewrites the base path from packages/app/dist/ to ./
+tar \
+  --no-xattrs \
+  -czf ./examples/medplum-provider/medplum-provider.tar.gz \
+  -C examples/medplum-provider/dist .
+
+
+# Build provider tarball
+# The -C flag rewrites the base path from packages/app/dist/ to ./
+tar \
+  --no-xattrs \
+  -czf ./examples/medplum-chart-demo/medplum-chart.tar.gz \
+  -C examples/medplum-chart-demo/dist .
+
+# Supply chain attestations
+# See: https://docs.docker.com/scout/policy/#supply-chain-attestations
+ATTESTATIONS="--provenance=true --sbom=true"
+
+# Target platforms
+PLATFORMS="--platform linux/amd64,linux/arm64"
+
+# If this is a release, get version information
+# Release is specified with a "--release" argument
+IS_RELEASE=false
+for arg in "$@"; do
+  if [[ "$arg" == "--release" ]]; then
+    IS_RELEASE=true
+    FULL_VERSION=$(node -p "require('./package.json').version")
+    MAJOR_DOT_MINOR=$(node -p "require('./package.json').version.split('.').slice(0, 2).join('.')")
+    break
+  fi
+done
+
+# Build and push server Docker images
+SERVER_TAGS="--tag $SERVER_DOCKERHUB_REPOSITORY:latest --tag $SERVER_DOCKERHUB_REPOSITORY:$GITHUB_SHA"
+if [[ "$IS_RELEASE" == "true" ]]; then
+  SERVER_TAGS="$SERVER_TAGS --tag $SERVER_DOCKERHUB_REPOSITORY:$FULL_VERSION --tag $SERVER_DOCKERHUB_REPOSITORY:$MAJOR_DOT_MINOR"
+fi
+docker buildx build $ATTESTATIONS $PLATFORMS $SERVER_TAGS --push .
+
+# Only build app if APP_DOCKERHUB_REPOSITORY was passed
+# This is so we can build the staging server Dockerfile without having to build app
+if [[ -n "${APP_DOCKERHUB_REPOSITORY}" ]]; then
+  # Build and push app Docker images
+  APP_TAGS="--tag $APP_DOCKERHUB_REPOSITORY:latest --tag $APP_DOCKERHUB_REPOSITORY:$GITHUB_SHA"
+  if [[ "$IS_RELEASE" == "true" ]]; then
+    APP_TAGS="$APP_TAGS --tag $APP_DOCKERHUB_REPOSITORY:$FULL_VERSION --tag $APP_DOCKERHUB_REPOSITORY:$MAJOR_DOT_MINOR"
+  fi
+  pushd packages/app
+  docker buildx build $ATTESTATIONS $PLATFORMS $APP_TAGS --push .
+  popd
+fi
+
+# Build and push foomedical Docker images
+FOOMEDICAL_TAGS="--tag $FOOMEDICAL_DOCKERHUB_REPOSITORY:latest --tag $FOOMEDICAL_DOCKERHUB_REPOSITORY:$GITHUB_SHA"
+if [[ "$IS_RELEASE" == "true" ]]; then
+  FOOMEDICAL_TAGS="$FOOMEDICAL_TAGS --tag $FOOMEDICAL_DOCKERHUB_REPOSITORY:$FULL_VERSION --tag $FOOMEDICAL_DOCKERHUB_REPOSITORY:$MAJOR_DOT_MINOR"
+fi
+pushd examples/foomedical
+docker buildx build $ATTESTATIONS $PLATFORMS $FOOMEDICAL_TAGS --push .
+popd
+
+
+# Build and push provider Docker images
+PROVIDER_TAGS="--tag $PROVIDER_DOCKERHUB_REPOSITORY:latest --tag $PROVIDER_DOCKERHUB_REPOSITORY:$GITHUB_SHA"
+if [[ "$IS_RELEASE" == "true" ]]; then
+  PROVIDER_TAGS="$PROVIDER_TAGS --tag $PROVIDER_DOCKERHUB_REPOSITORY:$FULL_VERSION --tag $PROVIDER_DOCKERHUB_REPOSITORY:$MAJOR_DOT_MINOR"
+fi
+pushd examples/medplum-provider
+docker buildx build $ATTESTATIONS $PLATFORMS $PROVIDER_TAGS --push .
+popd
+
+# Build and push chart Docker images
+CHART_TAGS="--tag $CHART_DOCKERHUB_REPOSITORY:latest --tag $CHART_DOCKERHUB_REPOSITORY:$GITHUB_SHA"
+if [[ "$IS_RELEASE" == "true" ]]; then
+  CHART_TAGS="$PROVIDER_TAGS --tag $CHART_DOCKERHUB_REPOSITORY:$FULL_VERSION --tag $CHART_DOCKERHUB_REPOSITORY:$MAJOR_DOT_MINOR"
+fi
+pushd examples/medplum-chart-demo
+docker buildx build $ATTESTATIONS $PLATFORMS $CHART_TAGS --push .
+popd
